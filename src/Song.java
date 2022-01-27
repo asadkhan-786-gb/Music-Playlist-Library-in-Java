5bfa67553e58c9745001bacd685a6f75c40566e8
asadkhan-786-gb <m.asadkhan079@gmail.com>
2022-01-27 14:32:23 +0500

    work Accomplish

---
+++ src/Song.java
@@ -0,0 +1,27 @@
+public class Song {
+    String title;
+    double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public Song() {
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" + "title='" + title + '\'' + ", duration=" 
+        + duration + '}';
+    }
+}
